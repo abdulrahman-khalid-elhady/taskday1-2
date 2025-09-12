8499d2c4eeff884689e2aa0fe365f20ed12b2436
boody <boody@LAPTOP-B172VK76>
2025-09-12 14:28:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+class Program
+{
+   
+    static void Main()
+    {
+        //--1
+        //Console.WriteLine((int)Convert.ToChar(Console.ReadLine()));
+
+        //--2
+        //Console.WriteLine((char)Convert.ToInt32(Console.ReadLine()));
+
+        //--3
+        //int n=Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine((n%2==0 ?"even":"odd"));
+
+        //--4
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine(a+b);
+        //Console.WriteLine(a-b);
+        //Console.WriteLine(a*b);
+
+        //--5
+        //int d = Convert.ToInt32(Console.ReadLine());
+        //if (d >= 90) Console.WriteLine("A");
+        //else if (d >= 75) Console.WriteLine("B");
+        //else if (d >= 60) Console.WriteLine("C");
+        //else if (d >= 50) Console.WriteLine("D");
+        //else Console.WriteLine("F");
+
+        //--6
+        //for (int i = 1; i < 13;i++)
+        //{
+        //    Console.WriteLine("Multi Table for : " + i);
+        //    for(int j = 1; j < 13;j++)
+        //    {
+        //        Console.WriteLine($"{i} x {j} = " + (i * j));
+        //    }
+        //}
+
+
+        //Task Day2
+
+        //--1
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //string[] NoS = new string[n];
+        //for (int i = 0; i < n; i++)
+        //{
+        //    NoS[i]= Console.ReadLine();
+        //}
+        //for (int i = 0; i < n; i++)
+        //{
+        //    Console.WriteLine(NoS[i]);
+        //}
+
+       // --2
+        //int s = Convert.ToInt32(Console.ReadLine());
+        //int t = Convert.ToInt32(Console.ReadLine());
+        //string[] st = new string[s];
+        //string[] tr = new string[s];
+        //int[] ages = new int[s];
+        //for (int i = 0; i < s; i++)
+        //{
+        //    st[i] = Console.ReadLine();
+        //    tr[i] = Console.ReadLine();
+        //    ages[i] = Convert.ToInt32(Console.ReadLine());
+
+        //}
+        //int[] avg = new int[s];
+        //int[] sum = new int[s];
+        //int[] num = new int[s];
+        //for (int i = 0; i < s; i++)
+        //{
+        //    for (int j = 0; j < s; j++)
+        //    {
+        //        if (tr[i] == tr[j]) { sum[i] += ages[j]; num[i]++; }
+        //    }
+
+        //}
+        //for (int i = 0; i < s; i++)
+        //{
+        //    bool f = false;
+        //    for (int j = 0; j < i; j++)
+        //    {
+        //        if (tr[i] == tr[j]) f = true;
+        //    }
+        //    if (num[i] != 0 && !f)
+        //    {
+        //        Console.WriteLine($"avg age of {tr[i]} is " + (sum[i] / num[i]));
+        //    }
+        //}
+
+        //--3
+
+        //*el class mkanh above el program class*
+        //    class time
+        //{
+        //    public
+        //    int h, m, s;
+        //}
+        //time t = new time();
+        //    t.h = Convert.ToInt32(Console.ReadLine());
+        //    t.m = Convert.ToInt32(Console.ReadLine());
+        //    t.s = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine($"{t.h}H:{t.m}M:{t.s}S");
+        //
+    }
+
+
+
+    
+}
+
